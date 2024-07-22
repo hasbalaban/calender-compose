54d186fa19b052d88fb359edf4ba4359031fa48f
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-07-22 16:48:58 +0300

    add github action yml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+
+    id("maven-publish")
 }
 
 android {
@@ -47,6 +53,60 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+
+}
+
+
+/**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN**/
+val githubProperties = Properties()
+githubProperties.load(FileInputStream(rootProject.file("github.properties")))
+
+fun getVersionName(): String {
+    return "1.0.2" // Replace with version Name
+}
+
+fun getArtificatId(): String {
+    return "sampleAndroidLib2" // Replace with library name ID
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            run {
+                groupId = "com.balaban.calender"
+                artifactId = getArtificatId()
+                version = getVersionName()
+                artifact("$buildDir/outputs/aar/${getArtificatId()}-release.aar")
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            /** Configure path of your package repository on Github
+             *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
+             */
+            url = uri("https://maven.pkg.github.com/enefce/AndroidLibrary-GPR-KDSL")
+            credentials {
+                /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN
+                 * OR
+                 * Set environment variables
+                 */
+                username = githubProperties.get("gpr.usr") as String? ?: System.getenv("GPR_USER")
+                password =
+                    githubProperties.get("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+
+            }
+        }
+    }
 }
 
 dependencies {
@@ -67,3 +127,5 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
+
+
