70e3a734dfc38a42df6f531781f89d92f4c3c87c
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-07-22 18:28:39 +0300

    change publish cred.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,9 @@ plugins {
 
 
     id("maven-publish")
+
+    `maven-publish`
+    signing
 }
 
 android {
@@ -76,38 +79,28 @@ fun getArtificatId(): String {
     return "sampleAndroidLib2" // Replace with library name ID
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("gpr") {
-            run {
-                groupId = "com.balaban.calender"
-                artifactId = getArtificatId()
-                version = getVersionName()
-                artifact("$buildDir/outputs/aar/${getArtificatId()}-release.aar")
-            }
-        }
-    }
 
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            /** Configure path of your package repository on Github
-             *  Replace GITHUB_USERID with your/organisation Github userID and REPOSITORY with the repository name on GitHub
-             */
-            url = uri("https://maven.pkg.github.com/enefce/AndroidLibrary-GPR-KDSL")
-            credentials {
-                /**Create github.properties in root project folder file with gpr.usr=GITHUB_USER_ID  & gpr.key=PERSONAL_ACCESS_TOKEN
-                 * OR
-                 * Set environment variables
-                 */
-                username = githubProperties.get("gpr.usr") as String? ?: System.getenv("GPR_USER")
-                password =
-                    githubProperties.get("gpr.key") as String? ?: System.getenv("GPR_API_KEY")
+group = "com.example"
+version = "1.0"
 
+
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.balaban"
+                artifactId = "calender"
+                version = "1.0.0"
             }
         }
     }
 }
+
+
+
 
 dependencies {
 
